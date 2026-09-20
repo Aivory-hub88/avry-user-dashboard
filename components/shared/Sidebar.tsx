285cdd127e51c67255d478586593ef1318ef91d6
Aivory-hub88 <irfan.reichmann@aivory.id>
2026-09-20 22:40:22 +0700

    style(sidebar): shrink logo 40\% (22px -> 13px)

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -255,9 +255,9 @@ export default function Sidebar() {
             <Image
               src={asset("/Aivory_logo_2026.svg?v=20260920")}
               alt="Aivory"
-              width={152}
-              height={22}
-              className="h-[22px] w-auto object-contain"
+              width={91}
+              height={13}
+              className="h-[13px] w-auto object-contain"
             />
           </div>
         ) : (
